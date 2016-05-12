f58c9cd01251ba5af791f0cf66b2caec9345d07a
Arnob L. Alam <arnoblalam@gmail.com>
2016-05-11 20:33:51 -0400

    Add util file

---
+++ src/utils.R
@@ -0,0 +1,13 @@
+# name          : utils.R
+# description   : Utility file (to load libraries etc)
+# maintainer    : Arnob L. Alam <aa2288a@student.american.edu>
+# updated       : 2016-05-11
+#
+# Utility file to load required libraries, helpful functions, etc.
+
+message("Processing util files")
+
+# Load the logging library
+library(futile.logger)
+flog.info("Loading required libraries")
+library(igraph)
