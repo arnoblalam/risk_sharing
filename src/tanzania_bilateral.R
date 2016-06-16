d4e32d516b84631d20f20c5206e426e599b53744
Arnob L. Alam <arnoblalam@gmail.com>
2016-06-15 23:59:24 -0400

    Get rid of some superfluous stuff

--- src/tanzania_bilateral.R
+++ src/tanzania_bilateral.R
@@ -12,66 +12,16 @@ flog.info("Reading in data")
 tanzania_data <- read.csv("data/tanzania_data.csv", stringsAsFactors = FALSE)
 
 flog.info("Converting to igraph structure")
-links <- as.matrix(tanzania_data[tanzania_data$willingness_link1 == 1, 1:2])
-tanzania_graph <- graph_from_edgelist(links,
-                                      directed = FALSE)
+# If either household says there is a link between them, we count it
+links <- as.matrix(tanzania_data[tanzania_data$willingness_link1 == 1 |
+                                     tanzania_data$willingness_link1 == 1
+                                 , 1:2])
+
+# Create an igraph undirected graph from the links
+tanzania_graph <- graph_from_edgelist(links, directed = FALSE)
+# Remove multiple and loops
 tanzania_graph <- simplify(tanzania_graph,
                            remove.multiple = TRUE,
                            remove.loops = TRUE)
 
 flog.info("Getting some basic statistics")
-length(V(tanzania_graph))
-
-connectedness <- degree(tanzania_graph)
-closeness_structure <- closeness(tanzania_graph)
-betweenness_structure <- betweenness(tanzania_graph)
-
-deg <- centr_degree(tanzania_graph)$centralization
-clos <- centr_clo(tanzania_graph)$centralization
-bet <- centr_betw(tanzania_graph)$centralization
-eig <- centr_eigen(tanzania_graph)$centralization
-
-# Print out the stats to screen
-message("The Tanzanaia data has ",
-        length(V(tanzania_graph)),
-        " nodes and ",
-        length(E(tanzania_graph)),
-        " connections (edges) between these nodes")
-message("The most connected household is (household ID) ",
-        which.max(connectedness),
-        " which has ", max(connectedness),
-        " connections")
-message("The household that is most frequently in the shortest path between two other households is household ",
-        which.max(betweenness_structure))
-message("The household that is adjacent to most other households is household ",
-        which.max(closeness_structure))
-
-message("
-        Measures of graph centralization (normalized to theoretical maximum of a 115 node network)")
-
-message("\tDegree Centralization: ", round(deg, 2))
-message("\tCloseness Centralization: ", round(clos, 2))
-message("\tBetweenness Centralization: ", round(bet), 2)
-message("\tEigenvector Centralization: ", round(eig), 2)
-
-message("Some measure of redunandancy:")
-
-mst <- minimum.spanning.tree(tanzania_graph)
-
-message("The minimum spanning tree for this graph has ", length(E(mst)),
-        " edges (compared to the " , length(E(tanzania_graph)),
-        " in the original graph).")
-
-
-flog.info("Plotting the graph and histogram of degree")
-
-plot(tanzania_graph, layout = layout_nicely)
-hist(connectedness,
-     main = "Histogram of degree distribution of the Tanzania Graph",
-     xlab = "Degrees")
-hist(closeness_structure,
-     main = "Histogram of closeness (1/distance) of the Tanzania Graph",
-     xlab = "Closeness")
-hist(betweenness_structure,
-     main = "Histogram of betweenness of the Tanzania Graph",
-     xlab = "Betweenness")
