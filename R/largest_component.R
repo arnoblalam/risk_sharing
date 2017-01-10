7567a1ef719aa675dcd8729c12f9e1c71c158554
Arnob Alam <arnoblalam@gmail.com>
2017-01-10 16:39:27 -0500

    Move largest component function to it's own file

---
+++ R/largest_component.R
@@ -0,0 +1,11 @@
+#' find the size of the giant component
+#'
+#' @param graph graph for which to return the giant component
+#'
+#' @return integer number of nodes in largest component of graph
+#' @export
+#'
+#' @examples
+largest_component <- function(graph) {
+    length(cluster.distribution(graph)) - 1
+}
